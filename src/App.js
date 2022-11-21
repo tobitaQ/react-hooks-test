30b135a602b6984225428ba03c4d6b4983b67cf3
tobitaQ <tobita-hiroaki@aiit.ac.jp>
2022-11-21 15:46:29 +0900

    useEffect examples

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const {name, price} = state;
 
+  useEffect(() => {
+    console.log('useeffect');
+  });
+
+  useEffect(() => {
+    console.log('useeffect');
+  }, []);
+
+
+  useEffect(() => {
+    console.log('useeffect');
+  }, [name]);
+
+
   return (
     <>
-    <p>現在の{name}は、{price}です!</p>
+    <p> 現在の{name}は、{price}です。 </p>
     <button onClick={()=> setState({...state, price: price+1})}>+1</button>
     <button onClick={()=> setState({...state, price: price-1})}>-1</button>    
     <button onClick={()=> setState(props)}>Reset</button>
